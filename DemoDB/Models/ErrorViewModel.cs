a29d360f1e2b2ae1b10a592d2daa929e3272c3fb
Anil Dhakad <anildhakad8587@gmail.com>
2025-06-03 10:11:38 +0530

    Add project files.

---
+++ DemoDB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
